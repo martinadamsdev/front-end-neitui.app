07ecf3641e3ac9d4942a3ac77f4118632f4e4849
martinageradams <w3cfed@gmail.com>
2021-04-13 15:33:43 +0800

    init commit

---
+++ src/components/Layout/app-aside.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "app-aside"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Layout/app-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "app-footer"
+}
+</script>
+
+<style scoped>
+
+</style>
